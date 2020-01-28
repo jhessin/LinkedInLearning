4410235a3e304dc7f002d9dce7b87108b2ff0bfd
Jim Hessin <jhessin@gmail.com>
2020-01-28 15:23:07 -0700

    Update to_do_list

--- Ruby_classes/Chapter_05/05_04-challenge/to_do_list/init.rb
+++ Ruby_classes/Chapter_05/05_04-challenge/to_do_list/init.rb
@@ -1,25 +1,26 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require_relative 'classes/to_do_list'
 
-list = ToDoList.new(['dishes', 'nap', 'laundry', 'vacuum'])
+list = ToDoList.new(%w[dishes nap laundry vacuum])
 
 puts
-puts "First: " + list.first
+puts 'First: ' + list.first
 puts
-puts "Longest: " + list.find {|item| item.length > 6 }
+puts 'Longest: ' + list.find { |item| item.length > 6 }
 puts
-puts "Original list:"
-puts list.map {|item| "> #{item.capitalize}" }
+puts 'Original list:'
+puts list.map { |item| "> #{item.capitalize}" }
 
-list << "sweep"
+list << 'sweep'
 puts
-puts "Updated list:"
-puts list.map {|item| "> #{item.capitalize}" }
+puts 'Updated list:'
+puts list.map { |item| "> #{item.capitalize}" }
 
 puts
-puts "Sorted:"
+puts 'Sorted:'
 list.sort.each_with_index do |item, i|
-  puts "#{i+1}: #{item}" 
+  puts "#{i + 1}: #{item}"
 end
 puts
