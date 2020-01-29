9a02aec2be07fb1b076807479af1d8c0415ab6e2
Jim Hessin <jhessin@gmail.com>
2020-01-28 18:03:03 -0700

    Update

--- Ruby_classes/project/errors.rb
+++ Ruby_classes/project/errors.rb
@@ -25,12 +25,27 @@ rescue ArgumentError => e
   puts 'Requires two integer arguments'
 end
 
+class NoEvenNumbers < StandardError
+  attr_accessor :array
+
+  def initialize(array)
+    super('No even numbers')
+    @array = array
+  end
+end
+
 def even_numbers(array)
   raise TypeError unless array.is_a?(Array)
   raise StandardError, 'Too few elements' if array.empty?
 
-  array.find_all { |el| el.to_i.even? }
+  even_array = array.find_all { |el| el.to_i.even? }
+
+  raise NoEvenNumbers.new(array) if even_array.empty?
 end
 
-puts even_numbers([1, 2, 3, 4, 5]).join(',')
-puts even_numbers(1..20).join(',')
+begin
+  puts even_numbers([1, 3, 5, 7]).join(',')
+rescue StandardError => e
+  puts "#{e.class}: #{e.message}"
+  puts "The array provided was: [#{e.array.join(', ')}]"
+end
