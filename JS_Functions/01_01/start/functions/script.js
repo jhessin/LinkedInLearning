8ec489e8db71978b527ced5e02807787455bd2e7
Jim Hessin <jhessin@gmail.com>
2020-02-14 07:36:54 -0700

    eslint setup

---
+++ JS_Functions/01_01/start/functions/script.js
@@ -0,0 +1,4 @@
+/* exported plus */
+var plus = function (a, b) {
+  return a + b;
+}(2,2)
---
+++ JS_Functions/01_03/start/functions/script.js
@@ -0,0 +1,7 @@
+function plus(a,b) {
+  return (
+  console.log(a+b),
+    console.log(this),
+    console.log(arguments)
+  )
+}
