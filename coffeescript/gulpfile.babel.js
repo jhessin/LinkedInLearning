86a17311033a6a61b094ad1f310088119fccbe57
Jim Hessin <jhessin@gmail.com>
2020-02-17 23:50:46 -0700

    General Updates
    added basic scss
    added yarn scripts

--- coffeescript/gulpfile.babel.js
+++ coffeescript/gulpfile.babel.js
@@ -2,23 +2,37 @@ import gulp from 'gulp';
 // import gutil from 'gulp-util'
 import uglify from 'gulp-uglify';
 import concat from 'gulp-concat';
+import sass from 'gulp-sass';
 
 let jsSources = [
   'components/scripts/scriptOne.js',
   'components/scripts/scriptTwo.js'
 ];
 
-export function js(cb) {
-  gulp.src(jsSources)
+let sassSources = [
+  'components/sass/*.scss'
+];
+
+export function js() {
+  return gulp.src(jsSources)
     .pipe(uglify())
     .pipe(concat('script.js'))
     .pipe(gulp.dest('js'));
+}
 
-  cb();
+export function css() {
+  return gulp.src(sassSources)
+    .pipe(sass({
+      style: 'expanded',
+      lineNumbers: true
+    }))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('css'));
 }
 
 export function watch() {
   gulp.watch(jsSources, gulp.parallel(js));
+  gulp.watch(sassSources, gulp.parallel(css));
 }
 
-gulp.task('default', gulp.series(js, watch));
+gulp.task('default', gulp.series(js, css, watch));
