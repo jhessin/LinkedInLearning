31ddf56d0cbf2f37639e68db365882229ceb373a
Jim Hessin <jhessin@gmail.com>
2020-02-27 14:40:26 -0700

    General Updates

--- rspec/car_project/spec/car_spec.rb
+++ rspec/car_project/spec/car_spec.rb
@@ -5,7 +5,11 @@ require 'car'
 
 describe 'Car' do # rubocop: disable Metrics/BlockLength
   describe 'attributes' do
-    let(:car) { Car.new }
+    subject { Car.new }
+
+    # use 'subject' instead of 'let'
+    # if variable is subject of example
+    # let(:car) { Car.new }
 
     # 'let' is better than 'before' for
     # setting up instance variables
@@ -14,22 +18,22 @@ describe 'Car' do # rubocop: disable Metrics/BlockLength
     # end
 
     it 'allows reading and writing for :make' do
-      car.make = 'Test'
-      expect(car.make).to eq('Test')
+      subject.make = 'Test'
+      expect(subject.make).to eq('Test')
     end
 
     it 'allows reading and writing for :year' do
-      car.year = 9999
-      expect(car.year).to eq(9999)
+      subject.year = 9999
+      expect(subject.year).to eq(9999)
     end
 
     it 'allows reading and writing for :color' do
-      car.color = 'foo'
-      expect(car.color).to eq('foo')
+      subject.color = 'foo'
+      expect(subject.color).to eq('foo')
     end
 
     it 'allows reading for :wheels' do
-      expect(car.wheels).to eq(4)
+      expect(subject.wheels).to eq(4)
     end
   end
 
@@ -50,7 +54,8 @@ describe 'Car' do # rubocop: disable Metrics/BlockLength
       )
     end
 
-    let(:car) { Car.new }
+    subject { Car.new }
+    # let(:car) { Car.new }
 
     it 'returns a string in the expected format' do
       expect(honda.full_name).to eq('2004 Honda (blue)')
@@ -58,7 +63,7 @@ describe 'Car' do # rubocop: disable Metrics/BlockLength
 
     context 'when initialized with no arguments' do
       it 'returns a string using default values' do
-        expect(car.full_name).to eq('2007 Volvo (unknown)')
+        expect(subject.full_name).to eq('2007 Volvo (unknown)')
       end
     end
   end
