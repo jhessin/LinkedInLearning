1f3c0961ab350dba61a888a9c8b442bd16eea9b6
Jim Hessin <jhessin@gmail.com>
2020-02-27 13:35:30 -0700

    General Updates

--- rspec/car_project/spec/car_spec.rb
+++ rspec/car_project/spec/car_spec.rb
@@ -5,50 +5,59 @@ require 'car'
 
 describe 'Car' do # rubocop: disable Metrics/BlockLength
   describe 'attributes' do
-    before :example do
-      @car = Car.new
-    end
+    let(:car) { Car.new }
+
+    # 'let' is better than 'before' for
+    # setting up instance variables
+    # before :example do
+    #   @car = Car.new
+    # end
 
     it 'allows reading and writing for :make' do
-      @car.make = 'Test'
-      expect(@car.make).to eq('Test')
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
     end
 
     it 'allows reading and writing for :year' do
-      @car.year = 9999
-      expect(@car.year).to eq(9999)
+      car.year = 9999
+      expect(car.year).to eq(9999)
     end
 
     it 'allows reading and writing for :color' do
-      @car.color = 'foo'
-      expect(@car.color).to eq('foo')
+      car.color = 'foo'
+      expect(car.color).to eq('foo')
     end
 
     it 'allows reading for :wheels' do
-      expect(@car.wheels).to eq(4)
+      expect(car.wheels).to eq(4)
     end
   end
 
   describe '.colors' do
+    let(:colors) { %w[blue black red green] }
+
     it 'returns an array of color names' do
-      c = %w[blue black red green]
-      expect(Car.colors).to match_array(c)
+      expect(Car.colors).to match_array(colors)
     end
   end
 
   describe '#full_name' do
-    it 'returns a string in the expected format' do
-      @honda = Car.new(
+    let(:honda) do
+      Car.new(
         make: 'Honda',
         year: 2004,
         color: 'blue'
       )
-      expect(@honda.full_name).to eq('2004 Honda (blue)')
+    end
+
+    let(:car) { Car.new }
+
+    it 'returns a string in the expected format' do
+      expect(honda.full_name).to eq('2004 Honda (blue)')
     end
 
     context 'when initialized with no arguments' do
       it 'returns a string using default values' do
-        car = Car.new
         expect(car.full_name).to eq('2007 Volvo (unknown)')
       end
     end
