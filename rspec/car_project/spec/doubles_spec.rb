e2bc0b8d68aff6c9416b5b6d03994bdd8bd7d5de
Jim Hessin <jhessin@gmail.com>
2020-03-01 22:26:53 -0700

    General Updates

--- rspec/car_project/spec/doubles_spec.rb
+++ rspec/car_project/spec/doubles_spec.rb
@@ -119,4 +119,32 @@ describe 'Doubles' do
       expect(customers[1].name).to eq('Mary')
     end
   end
+
+  context 'with message expectations' do
+    it 'expects a call and allows a response' do
+      dbl = double('Chant')
+      expect(dbl).to receive(:hey!).and_return('Ho!')
+      dbl.hey!
+    end
+
+    it 'does not matter which order' do
+      dbl = double('Multi-step Process')
+
+      expect(dbl).to receive(:step_1)
+      expect(dbl).to receive(:step_2)
+
+      dbl.step_2
+      dbl.step_1
+    end
+
+    it 'works with #ordered when order matters' do
+      dbl = double('Multi-step Process')
+
+      expect(dbl).to receive(:step_1).ordered
+      expect(dbl).to receive(:step_2).ordered
+
+      dbl.step_1
+      dbl.step_2
+    end
+  end
 end
