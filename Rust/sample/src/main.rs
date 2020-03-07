6b99c52d175af3015c7477c3a2068fdce21c703b
Jim Hessin <jhessin@gmail.com>
2020-03-06 20:13:49 -0700

    vifm Generated Update

--- Rust/sample/src/main.rs
+++ Rust/sample/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     another_function();
 
     another_function2(5);
+
+    let a = sum(10, 5);
+
+    println!("Sum = {}", a);
 }
 
 fn another_function() {
@@ -10,4 +14,13 @@ fn another_function() {
 
 fn another_function2(x: i32) {
     println!("The value of x = {}", x);
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    let mut b = x + y;
+
+    b = b * 100;
+
+    b // Equivalent v
+      // return b;
 }
