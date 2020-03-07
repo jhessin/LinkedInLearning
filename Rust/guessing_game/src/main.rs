b6a089ba2f4eb3d0afb2348599fa3692894b2c66
Jim Hessin <jhessin@gmail.com>
2020-03-07 16:02:03 -0700

    vifm Generated Update

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,3 +1,35 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to my guessing game!",);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("Secret number is = {}", secret_number);
+
+    loop {
+        println!("Please input your guess...",);
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = guess.trim().parse().expect("Numbers only please");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!",),
+            Ordering::Greater => println!("Too Big!",),
+            Ordering::Equal => {
+                println!("You win!",);
+                break;
+            }
+        }
+    }
 }
