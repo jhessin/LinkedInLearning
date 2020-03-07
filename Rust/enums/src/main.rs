a3afc8a243ffae293868c45e8ed1c5952a7bcf68
Jim Hessin <jhessin@gmail.com>
2020-03-07 15:03:39 -0700

    vifm Generated Update

--- Rust/enums/src/main.rs
+++ Rust/enums/src/main.rs
@@ -3,10 +3,26 @@ enum IpAddrKind {
     V6(String),
 }
 
-fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+#[allow(dead_code)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
-    let home = IpAddrKind::V4(String::from("127.0.0.1"));
-    let loopback = IpAddrKind::V6(String::from("::1"));
+impl Message {
+    fn call(&self) {
+        println!("I'm inside Call!",);
+    }
+}
+fn main() {
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
+
+    let _home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let _loopback = IpAddrKind::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
