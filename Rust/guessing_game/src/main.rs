b0803280528973273384961c720987b3f9959ac2
Jim Hessin <jhessin@gmail.com>
2020-03-07 16:43:23 -0700

    General Updates

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -5,31 +5,29 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Welcome to my guessing game!",);
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+  println!("Welcome to my guessing game!",);
+  let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    // println!("Secret number is = {}", secret_number);
+  // println!("Secret number is = {}", secret_number);
 
-    loop {
-        println!("Please input your guess...",);
+  loop {
+    println!("Please input your guess...",);
 
-        let mut guess = String::new();
+    let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line!");
+    io::stdin().read_line(&mut guess).expect("Failed to read line!");
 
-        println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess);
 
-        let guess: u32 = guess.trim().parse().expect("Numbers only please");
+    let guess: u32 = guess.trim().parse().expect("Numbers only please");
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!",),
-            Ordering::Greater => println!("Too Big!",),
-            Ordering::Equal => {
-                println!("You win!",);
-                break;
-            }
-        }
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!",),
+      Ordering::Greater => println!("Too Big!",),
+      Ordering::Equal => {
+        println!("You win!",);
+        break;
+      }
     }
+  }
 }
