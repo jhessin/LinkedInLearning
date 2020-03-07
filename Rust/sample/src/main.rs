9dc529f5f348ef3c7f3b07b3b923d33334b17802
Jim Hessin <jhessin@gmail.com>
2020-03-06 20:07:48 -0700

    vifm Generated Update

--- Rust/sample/src/main.rs
+++ Rust/sample/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     another_function();
+
+    another_function2(5);
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn another_function2(x: i32) {
+    println!("The value of x = {}", x);
 }
