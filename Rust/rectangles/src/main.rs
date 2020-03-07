572975e847632cedca094c3c9e83fb6248726434
Jim Hessin <jhessin@gmail.com>
2020-03-07 14:22:32 -0700

    vifm Generated Update

--- Rust/rectangles/src/main.rs
+++ Rust/rectangles/src/main.rs
@@ -1,3 +1,21 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 fn main() {
-    println!("Hello, world!");
+    // let width = 30;
+    // let height = 50;
+
+    let rect1 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+
+    // println!("Area is {}", area(width, height));
+
+    println!("Area is {}", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
